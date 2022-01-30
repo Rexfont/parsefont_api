10694579b2d123de1435e931f49f8a6bee116e71
aXoncodes <devops@axoncodes.com>
2022-01-31 02:08:01 +0330

    Complete the process of recieving the files getting them to zip and downloading to client

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const multer  = require('multer')
+const cors = require('cors')
+app.use(cors())
+const upload = multer({ dest: '../uploads/' })
+const parsefont = require('parsefont');
+const JSZip = require('jszip');
+const parsefontRoot = '/parsefont';
+const PORT = process.env.PORT || 8002;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} : ${req._parsedUrl.path}`);
+  next();
+})
+
+app.get(`${parsefontRoot}/`, (req, res) => {
+  res.sendFile(path.join(__dirname, `./index.html`));
+})
+
+app.post(`${parsefontRoot}/convert`, upload.single('file'), (req, res) => {
+  parsefont.get({ svgFile: path.join(__dirname, `./${req.file.path}`), ...req.body })
+  .then(fiels => {
+    var zip = new JSZip();
+    zip.file(`index.html`, fiels.html);
+    zip.file(`style.css`, fiels.style);
+    zip.file(`fontsvg.svg`, fiels.fontSvg);
+    zip.file(`font.ttf`, fiels.svg2ttfbuf);
+    zip.file(`font.eot`, fiels.svg2eotbuf);
+    zip.file(`font.woff`, fiels.svg2woffbuf);
+    return zip.generateAsync({ type: "base64" });
+  })
+  .then(response => res.send(response))
+  .catch(error => {
+    console.error(error.stack?error.stack:error);
+    res.status(400).send(error.stack?error.stack:error)
+  });
+})
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
